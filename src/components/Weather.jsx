e49c4472c29b1fedac8f50059d10194072220da7
Tito <innowhat@gmail.com>
2019-08-07 16:15:37 +0300

    Search function

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,43 +6,71 @@ import Heading from "../elements/Heading";
 import Card from "../elements/Card";
 import WeatherInput from "./WeatherInput";
 
+const API_KEY = "ad1d890318187965928b47ae73fc7bc6";
+
 class Weather extends React.Component {
-  render() {
-    const randomCards = [];
+  constructor() {
+    super();
+    this.state = {
+      city: "",
+      temperature: "",
+      temp_high: "",
+      temp_low: "",
+      humidity: "",
+      description: "",
+      isLoading: true
+    };
+  }
 
-    for (let i = 0; i < 4; i++) {
-      randomCards.push({
-        date: faker.date.weekday(1)
+  componentDidMount() {}
+
+  handleSearch = event => {
+    event.preventDefault();
+
+    const city = event.target.city.value;
+    const country = event.target.country.value;
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    )
+      .then(response => response.json())
+      .then(responseData => {
+        console.log(responseData);
+        this.setState({
+          city: responseData.name,
+          temperature: responseData.main.temp,
+          temp_high: responseData.main.temp_max,
+          temp_low: responseData.main.temp_min,
+          humidity: responseData.main.humidity,
+          description: responseData.weather[0].description,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
       });
-    }
+  };
 
+  render() {
     return (
       <div>
         <Container>
           <Flex>
-            <Card big style={{ width: "100%" }}>
-              <Heading h3>Choose City or Country</Heading>
-              <WeatherInput />
-            </Card>
+            <WeatherInput handleSearch={this.handleSearch} />
           </Flex>
         </Container>
         <Container>
           <Flex>
-            {randomCards.map((el, index) => {
-              return (
-                <Card
-                  primary
-                  key={index}
-                  delay={index * 125}
-                  style={{ width: "100%" }}
-                >
-                  <Heading style={{ color: "#fff" }} h4>
-                    {el.date}
-                  </Heading>
-                  <p>{el.copy}</p>
-                </Card>
-              );
-            })}
+            <Card primary delay={125} style={{ width: "100%" }}>
+              <Heading style={{ color: "#fff" }} h4>
+                <h3>{this.state.city}</h3>
+                <h1>{this.state.temperature}</h1>
+                <h3>{this.state.humidity}</h3>
+                <p>
+                  {this.state.temp_low}, {this.state.temp_high}
+                </p>
+                <p>{this.state.description}</p>
+              </Heading>
+            </Card>
           </Flex>
         </Container>
       </div>
--- src/components/WeatherInput.jsx
+++ src/components/WeatherInput.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Button from "../elements/Button";
+import Card from "../elements/Card";
+import Heading from "../elements/Heading";
 
-function WeatherInput() {
+function WeatherInput(props) {
   return (
-    <form>
-      <input type="text" name="city" placeholder="Enter city" />
-    </form>
+    <Card big style={{ width: "100%" }}>
+      <Heading h3>Search by City or Country</Heading>
+      <form onSubmit={props.handleSearch}>
+        <input
+          style={{ width: "50%", padding: "20px", fontSize: "40px" }}
+          type="text"
+          name="city"
+          placeholder="City"
+        />
+        <input
+          style={{ width: "50%", padding: "20px", fontSize: "40px" }}
+          type="text"
+          name="country"
+          placeholder="Country "
+        />
+        <Button
+          secondary
+          type="submit"
+          name="btnSearch"
+          style={{ width: "100%", padding: "20px", fontSize: "40px" }}
+        >
+          Search
+        </Button>
+      </form>
+    </Card>
   );
 }
 
