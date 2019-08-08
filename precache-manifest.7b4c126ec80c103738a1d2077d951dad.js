2c98277bdb9d28cf19ac510422b094af2fcf881a
Tito <innowhat@gmail.com>
2019-08-08 17:15:28 +0300

    Updates

---
+++ precache-manifest.7b4c126ec80c103738a1d2077d951dad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c1b2b5414af2553e22ee5b4ac4a1cd",
+    "url": "/vnicorn-weather-app/index.html"
+  },
+  {
+    "revision": "2b313e32142e50f3ab37",
+    "url": "/vnicorn-weather-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "be5543a72603c1688f01",
+    "url": "/vnicorn-weather-app/static/js/2.87572cad.chunk.js"
+  },
+  {
+    "revision": "2b313e32142e50f3ab37",
+    "url": "/vnicorn-weather-app/static/js/main.2cce3c3e.chunk.js"
+  },
+  {
+    "revision": "50d5e9b07a0c1ed30ad4",
+    "url": "/vnicorn-weather-app/static/js/runtime~main.6bdfd8e3.js"
+  }
+]);
